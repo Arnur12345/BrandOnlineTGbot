ed443cac4eb5890782ba9b6d73375feb3f103ebf
Arnur12345 <dominusvf@gmail.com>
2024-07-11 16:52:16 +0500

    s

--- myapp/views.py
+++ myapp/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
+
 
 def user_login(request):
     if request.method == 'POST':
@@ -27,7 +29,7 @@ def user_logout(request):
     return redirect('login')
 
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 def dashboard(request):
     user_count = Users.objects.count()
     test_count = Tests.objects.count()
@@ -41,183 +43,183 @@ def dashboard(request):
     }
     return render(request, 'dashboard.html', context)
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class UserListView(ListView):
     model = Users
     template_name = 'user_list.html'
     context_object_name = 'users'
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class UserCreateView(CreateView):
     model = Users
     form_class = UserForm
     template_name = 'user_form.html'
     success_url = reverse_lazy('user_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class UserUpdateView(UpdateView):
     model = Users
     form_class = UserForm
     template_name = 'user_form.html'
     success_url = reverse_lazy('user_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class UserDeleteView(DeleteView):
     model = Users
     template_name = 'user_confirm_delete.html'
     success_url = reverse_lazy('user_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class SubjectListView(ListView):
     model = Subject
     template_name = 'subject_list.html'
     context_object_name = 'subjects'
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class SubjectCreateView(CreateView):
     model = Subject
     form_class = SubjectForm
     template_name = 'subject_form.html'
     success_url = reverse_lazy('subject_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class SubjectUpdateView(UpdateView):
     model = Subject
     form_class = SubjectForm
     template_name = 'subject_form.html'
     success_url = reverse_lazy('subject_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class SubjectDeleteView(DeleteView):
     model = Subject
     template_name = 'subject_confirm_delete.html'
     success_url = reverse_lazy('subject_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class TestListView(ListView):
     model = Tests
     template_name = 'test_list.html'
     context_object_name = 'tests'
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class TestCreateView(CreateView):
     model = Tests
     form_class = TestForm
     template_name = 'test_form.html'
     success_url = reverse_lazy('test_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class TestUpdateView(UpdateView):
     model = Tests
     form_class = TestForm
     template_name = 'test_form.html'
     success_url = reverse_lazy('test_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class TestDeleteView(DeleteView):
     model = Tests
     template_name = 'test_confirm_delete.html'
     success_url = reverse_lazy('test_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class QuestionListView(ListView):
     model = Questions
     template_name = 'question_list.html'
     context_object_name = 'questions'
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class QuestionCreateView(CreateView):
     model = Questions
     form_class = QuestionForm
     template_name = 'question_form.html'
     success_url = reverse_lazy('question_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class QuestionUpdateView(UpdateView):
     model = Questions
     form_class = QuestionForm
     template_name = 'question_form.html'
     success_url = reverse_lazy('question_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class QuestionDeleteView(DeleteView):
     model = Questions
     template_name = 'question_confirm_delete.html'
     success_url = reverse_lazy('question_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class OptionListView(ListView):
     model = Options
     template_name = 'option_list.html'
     context_object_name = 'options'
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class OptionCreateView(CreateView):
     model = Options
     form_class = OptionForm
     template_name = 'option_form.html'
     success_url = reverse_lazy('option_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class OptionUpdateView(UpdateView):
     model = Options
     form_class = OptionForm
     template_name = 'option_form.html'
     success_url = reverse_lazy('option_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class OptionDeleteView(DeleteView):
     model = Options
     template_name = 'option_confirm_delete.html'
     success_url = reverse_lazy('option_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class UserAnswerListView(ListView):
     model = UserAnswer
     template_name = 'user_answer_list.html'
     context_object_name = 'user_answers'
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class UserAnswerCreateView(CreateView):
     model = UserAnswer
     form_class = UserAnswerForm
     template_name = 'user_answer_form.html'
     success_url = reverse_lazy('user_answer_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class UserAnswerUpdateView(UpdateView):
     model = UserAnswer
     form_class = UserAnswerForm
     template_name = 'user_answer_form.html'
     success_url = reverse_lazy('user_answer_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class UserAnswerDeleteView(DeleteView):
     model = UserAnswer
     template_name = 'user_answer_confirm_delete.html'
     success_url = reverse_lazy('user_answer_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class UserPerformanceListView(ListView):
     model = UserPerformance
     template_name = 'user_performance_list.html'
     context_object_name = 'user_performances'
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class UserPerformanceCreateView(CreateView):
     model = UserPerformance
     form_class = UserPerformanceForm
     template_name = 'user_performance_form.html'
     success_url = reverse_lazy('user_performance_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class UserPerformanceUpdateView(UpdateView):
     model = UserPerformance
     form_class = UserPerformanceForm
     template_name = 'user_performance_form.html'
     success_url = reverse_lazy('user_performance_list')
 
-@login_required
+@method_decorator(login_required, name='dispatch')
 class UserPerformanceDeleteView(DeleteView):
     model = UserPerformance
     template_name = 'user_performance_confirm_delete.html'
