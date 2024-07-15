ca42aa44eb63551a169e1d1ce2bf7c7950a48427
Arnur12345 <dominusvf@gmail.com>
2024-07-15 15:32:06 +0500

    sasa

--- myapp/views.py
+++ myapp/views.py
@@ -118,15 +118,15 @@ class TestDeleteView(DeleteView):
     template_name = 'test_confirm_delete.html'
     success_url = reverse_lazy('test_list')
 
-    class TestDetailView(DetailView):
-        model = Tests
-        template_name = 'test_detail.html'
-        context_object_name = 'test'
+class TestDetailView(DetailView):
+    model = Tests
+    template_name = 'test_detail.html'
+    context_object_name = 'test'
 
-        def get_context_data(self, **kwargs):
-            context = super().get_context_data(**kwargs)
-            context['questions'] = Questions.objects.filter(test=self.object)
-            return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['questions'] = Questions.objects.filter(test=self.object)
+        return context
 
 
 class QuestionListView(ListView):
